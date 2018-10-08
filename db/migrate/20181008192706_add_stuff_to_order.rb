01d354b89c745b1f20c45071c8dfeb451c6fc1c9
Adrien Lefrancq <irdane2111@gmail.com>
2018-10-08 15:06:19 -0500

    add column database

--- db/migrate/20181008192706_add_stuff_to_order.rb
+++ db/migrate/20181008192706_add_stuff_to_order.rb
@@ -2,7 +2,7 @@ class AddStuffToOrder < ActiveRecord::Migration[5.2]
   def change
     add_column :orders, :status, :integer
     add_column :orders, :shippeddate, :string
-    add_column :orders, :amount, :monetize
-    add_references :orders, :users, foreign_key: true
+    add_monertize :orders, :amount
+    add_reference :orders, :user, foreign_key: true
   end
 end
