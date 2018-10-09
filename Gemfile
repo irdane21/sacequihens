a048c1117a6fc100b289006dd7a24d29aa2c9858
Adrien Lefrancq <irdane2111@gmail.com>
2018-10-09 15:54:42 -0500

    WIP pundit

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'uglifier'
 gem 'webpacker'
 gem 'money-rails'
 gem 'stripe'
+gem 'pundit'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,25 +2,34 @@ class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :update, :edit]
 
   def index
-    @products = Product.all
+    @product = policy_scope(Product)
   end
 
   def show
+    authorize @product
   end
 
   def new
     @product = Product.new
+    authorize @product
   end
 
   def create
     @product = Product.new(params_product)
-    @product.save
+    authorize @product
+    if @product.save
+      redirect_to dashboard_path(current_user)
+    else
+      render :new
+    end
   end
 
   def edit
+    authorize @product
   end
 
   def update
+    authorize @product
     if @product.update(params_product)
       redirect_to
     else
